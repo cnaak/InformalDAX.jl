7fabb51e9d82a4c657218461af0cfe1a60401cd5
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-04-21 15:55:00 -0300

    UPD the src/statement.jl

--- src/statement.jl
+++ src/statement.jl
@@ -3,5 +3,26 @@
 A data structure representing a generic (any operation type) statement line.
 """
 struct GenericStatementLine <: AbstractStatementLine
+    HistoryTradesWsData::String
+    TransactionType::String
+    TransactionCoin::String
+    TransactionAmount::String
+    TransactionOutcome::String
+    GenericStatementLine(RawStatementLine::String) = begin
+        s = split(RawStatementLine, ',')
+        if length(s) == 6
+            HistoryTradesWsData, \
+            TransactionType, \
+            TransactionCoin, \
+            TransactionAmount \
+            TransactionOutcome = s[1], s[2], s[3], join(s[4:5], ','), s[6]
+        elseif length(s) == 5
+            HistoryTradesWsData, \
+            TransactionType, \
+            TransactionCoin, \
+            TransactionAmount \
+            TransactionOutcome = s
+        end
+    end
 end
 
