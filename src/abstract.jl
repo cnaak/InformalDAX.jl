70e1fc5e5a191cfca43868401e6ef80026c2c3e6
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-04-24 17:40:13 -0300

    UPD the src/abstract.jl

--- src/abstract.jl
+++ src/abstract.jl
@@ -30,13 +30,41 @@ Second-level Balance abstract supertype.
 abstract type AbstractBalance <: Informal_DAX end
 
 
-# Third-level abstract types
-# --------------------------
+# Third-level abstract types <: AbstractStatement
+# -----------------------------------------------
 
 """
 `abstract type AbstractStatementLine <: AbstractStatement end`\n
 Third-level StatementLine abstract supertype.
 """
 abstract type AbstractStatementLine <: AbstractStatement end
+
+
+# Third+ -level abstract types <: AbstractBalance
+# -----------------------------------------------
+
+"""
+`abstract type Untrakd <: AbstractBalance end`\n
+Untracked single balances
+"""
+abstract type Untrakd <: AbstractBalance end
+
+"""
+`abstract type Tracked <: AbstractBalance end`\n
+Fiat-Tracked balances.
+"""
+abstract type Tracked <: AbstractBalance end
+
+"""
+`abstract type UniTracked <: Tracked end`\n
+Fiat-Tracked, single balance.
+"""
+abstract type UniTracked <: Tracked end
+
+"""
+`abstract type MulTracked <: Tracked end`\n
+Fiat-Tracked, multi balance.
+"""
+abstract type MulTracked <: Tracked end
 
 
