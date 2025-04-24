f69d19840d41bd1913db46cf4f5fc6c8414f63c0
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-04-24 18:50:09 -0300

    UPD the src/finance.jl

--- src/finance.jl
+++ src/finance.jl
@@ -7,13 +7,8 @@ SFD =  FixedDecimal{Int64,10}   # from -922337203.6854775808
                                 # upto          0.0000000001
                                 # upto  922337203.6854775807
 
-UFD = FixedDecimal{UInt64,10}   # from          0.0000000000
-                                # upto          0.0000000001
-                                # upto  922337203.6854775807
-                                # upto 1844674407.3709551615 (not meant to be used for compat)
+INPUTS = Union{SFD, Rational, Integer}
 
-INPUTS = Union{UFD, SFD, Rational, Integer}
-
-export UFD, SFD, INPUTS
+export SFD, INPUTS
 
 
