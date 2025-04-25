bc97584ac27edbbf0bfb8018e49d1acb3735c097
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-04-24 23:10:32 -0300

    UPD the src/finance.jl

--- src/finance.jl
+++ src/finance.jl
@@ -7,9 +7,9 @@ SFD =  FixedDecimal{Int64,10}   # from -922337203.6854775808
                                 # upto          0.0000000001
                                 # upto  922337203.6854775807
 
-INPUTS = Union{SFD, Rational, Integer}
+DECIM = Union{SFD, Rational, Integer}
 
 # export
-export SFD, INPUTS
+export SFD, DECIM
 
 
