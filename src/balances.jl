6698c94816ebc1b906d8e94c92f8195819e75913
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-04-25 00:25:01 -0300

    UPD the src/balances.jl

--- src/balances.jl
+++ src/balances.jl
@@ -74,7 +74,7 @@ struct STB <: UniTracked
     fiat::SUB
     function STB(CRYP::SUB, FIAT::SUB)
         @assert(isFiat(FIAT), "Tracking must be against a Fiat currency!")
-        @assert((bare(CRYP) == zero(SFD)) || (bare(FIAT) == zero(SFD)),
+        @assert((bare(CRYP) != zero(SFD)) && (bare(FIAT) != zero(SFD)),
                 "Exchange ratio must be finite!")
         new(CRYP, FIAT)
     end
