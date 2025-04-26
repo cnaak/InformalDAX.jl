ba5990ffd0e16aa8bdf8b9a86cab3ca661c16b52
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-04-26 14:49:30 -0300

    UPD the src/operations.jl

--- src/operations.jl
+++ src/operations.jl
@@ -2,8 +2,26 @@
 #                      operations.jl - balance-changing statement operations                       #
 #--------------------------------------------------------------------------------------------------#
 
-"""
+# RFB's fiat currency
+RFBFiat = :BRL
+
+# export
+export RFBFiat
+
+# Empty balance initializer with :BRL as tracking fiat
+emptyRFB() = MTB(STB((RFBFiat, RFBFiat)))
+
+
+#--------------------------------------------------------------------------------------------------#
+#                                       Operation Functions                                        #
+#--------------------------------------------------------------------------------------------------#
 
 """
-function 𝑜Init(prev::MTB = MTB())
+`𝑜Init(prev::MTB = emptyRFB())`\n
+Initialization operation, for a new month's statement processing. Argument is previous month's
+balance. If ommitted, defaults to an emptyRFB() one.
+"""
+function 𝑜Init(prev::MTB = emptyRFB())
+    return prev
+end
 
