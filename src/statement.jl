ca9ec7a5a62675c7ae0a0b94eb01b1c56a96c2bf
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-04-27 17:05:34 -0300

    UPD the src/statement.jl

--- src/statement.jl
+++ src/statement.jl
@@ -6,20 +6,23 @@ import Base: show
 
 
 #--------------------------------------------------------------------------------------------------#
-#                                       GenericStatementLine                                       #
+#                                             GenSTLn                                              #
 #--------------------------------------------------------------------------------------------------#
 
 """
-`struct GenericStatementLine <: AbstractStatementLine`\n
+`struct GenSTLn <: AbstractSTLn`\n
 A data structure representing a generic (any operation type) statement line.
 """
-struct GenericStatementLine <: AbstractStatementLine
-    HistoryTradesWsData::String
-    TransactionType::String
-    TransactionCoin::String
-    TransactionAmount::String
-    TransactionOutcome::String
-    GenericStatementLine(RawStatementLine::AbstractString) = begin
+struct GenSTLn <: AbstractSTLn
+    dat::String
+    typ::String
+    coi::String
+    amt::String
+    out::String
+    # Components constructor
+    GenSTLn(cmp::NTuple{5,String}) = new(cmp...)
+    # Whole line constructor
+    GenSTLn(RawStatementLine::AbstractString) = begin
         if length(strip(RawStatementLine)) == 0
             return new("", "", "", "", "")
         end
@@ -28,45 +31,44 @@ struct GenericStatementLine <: AbstractStatementLine
         tmp = join([noCGr("dat"), noCGr("typ"), noCGr("coi"), noQGr("amt"), noCGr("out")], ",")
         rex = Regex(join(["^", tmp, "\$"]))
         m = match(rex, RawStatementLine)
+        @assert(m != nothing, "Couldn't parse the statement line:\n>>> $(RawStatementLine) <<<")
         new(m[:dat], m[:typ], m[:coi], m[:amt], m[:out])
     end
 end
 
 # export
-export GenericStatementLine
+export GenSTLn
 
-function Base.show(io::IO, ::MIME"text/plain", gl::GenericStatementLine)
-    WBOLD = Crayon(foreground = :white, bold = true, background = :dark_gray)
-    FAINT = Crayon(foreground = :light_gray, bold = false, background = :default)
-    RESET = Crayon(reset = true)
-    print(WBOLD, "Date:", FAINT, @sprintf("%20s ", gl.HistoryTradesWsData))
-    print(WBOLD, "Type:", FAINT, @sprintf("%22s ", gl.TransactionType    ))
-    print(WBOLD, "Coin:", FAINT, @sprintf("%10s ", gl.TransactionCoin    ))
-    print(WBOLD, "Amnt:", FAINT, @sprintf("%38s ", gl.TransactionAmount  ))
-    print(WBOLD, "Outc:", FAINT, @sprintf("%08s.", gl.TransactionOutcome ))
-    print(RESET)
+raw(x::GenSTLn) = join([x.dat, x.typ, x.coi,
+                        occursin(',', x.amt) ? "\"$(x.amt)\"" : x.amt,
+                        x.out], ",")
+# export
+export raw
+
+function Base.show(io::IO, ::MIME"text/plain", x::GenSTLn)
+    print(@sprintf("GenSTLn(%s)", repr(raw(x))))
 end
 
 
 #--------------------------------------------------------------------------------------------------#
-#                                          ParsedStmtLine                                          #
+#                                             ParSTLn                                              #
 #--------------------------------------------------------------------------------------------------#
 
-struct ParsedStmtLine <: AbstractStatementLine
+struct ParSTLn <: AbstractSTLn
     DATE::DateTime
     TYPE::Tuple{String, String}
     COIN::Symbol
     AMNT::Tuple{Bool, SFD, Symbol}
     OUTC::Bool
-    function ParsedStmtLine(g::GenericStatementLine)
+    function ParSTLn(g::GenSTLn)
         # Early exit for empty/last statement lines
-        if length(g.HistoryTradesWsData) == 0
+        if length(g.dat) == 0
             return new(DateTime(0, 1, 1, 0, 0, 0), ("Header", ""),
                        :nothing, (false, zero(SFD), :nothing), false)
-        elseif (g.TransactionType    == "Type"   || 
-                g.TransactionCoin    == "Coin"   ||
-                g.TransactionAmount  == "Amount" ||
-                g.TransactionOutcome == "Status")
+        elseif (g.typ    == "Type"   || 
+                g.coi    == "Coin"   ||
+                g.amt  == "Amount" ||
+                g.out == "Status")
             return new(DateTime(0, 1, 1, 0, 0, 0), ("Footer", ""), 
                        :nothing, (false, zero(SFD), :nothing), false)
         end
@@ -74,7 +76,7 @@ struct ParsedStmtLine <: AbstractStatementLine
         dex  = raw"^(?<MM>[0-9]{2})/(?<DD>[0-9]{2})/(?<YY>[0-9]{4})"
         tex  = raw" (?<hh>[0-9]{2}):(?<mm>[0-9]{2}):(?<ss>[0-9]{2})"
         rex  = Regex(join([dex, tex], ""))
-        m    = match(rex, g.HistoryTradesWsData)
+        m    = match(rex, g.dat)
         if m isa Nothing
             return new(
                 DateTime(0, 1, 1, 0, 0, 0),
@@ -97,7 +99,7 @@ struct ParsedStmtLine <: AbstractStatementLine
             )
         )
         # Type parsing
-        splt = split(g.TransactionType, ' ')
+        splt = split(g.typ, ' ')
         if length(splt) >= 3
             𝑡𝑦𝑝𝑒 = (string(splt[1]), string(splt[end]))
         elseif length(splt) == 2
@@ -111,13 +113,13 @@ struct ParsedStmtLine <: AbstractStatementLine
             end
         end
         # Coin parsing
-        coin = Symbol(strip(g.TransactionCoin))
+        coin = Symbol(strip(g.coi))
         # Amount parsing
         dash = "-\u2010\u2011\u2012\u2013\u2014\u2015\ufe58\ufe63\uff0d\u2e3a\u2e3b"
-        if startswith(raw"R$")(g.TransactionAmount)
+        if startswith(raw"R$")(g.amt)
             # R$ (BRL) parsing
             rex  = r"R\$ ?(?<sig>[+-]?)(?<val>[0-9.,]+)"
-            m    = match(rex, g.TransactionAmount)
+            m    = match(rex, g.amt)
             if m isa Nothing
                 return new(date, 𝑡𝑦𝑝𝑒, coin, (false, zero(SFD), :nothing), false)
             end
@@ -136,7 +138,7 @@ struct ParsedStmtLine <: AbstractStatementLine
         else
             # Other parsing
             rex  = r"^(?<sig>[+-]?)(?<val>[0-9.,]+) ?(?<cur>[A-Z]+)"
-            m    = match(rex, g.TransactionAmount)
+            m    = match(rex, g.amt)
             if m isa Nothing
                 return new(date, 𝑡𝑦𝑝𝑒, coin, (false, zero(SFD), :nothing), false)
             end
@@ -155,19 +157,50 @@ struct ParsedStmtLine <: AbstractStatementLine
             amnt = (sbt, SFD(NUME//DENO), Symbol(cur))
         end
         # Outcome parsing
-        outc = g.TransactionOutcome == "Success"
+        outc = g.out == "Success"
         # Final assembly
         new(date, 𝑡𝑦𝑝𝑒, coin, amnt, outc)
     end
 end
 
-# export
-export ParsedStmtLine
+# functor
+(x::ParSTLn)() = x.DATE, x.TYPE, x.COIN, x.AMNT, x.OUTC
 
-function Base.show(io::IO, ::MIME"text/plain", pl::ParsedStmtLine)
+# export
+export ParSTLn
+
+function Base.show(io::IO, ::MIME"text/plain", pl::ParSTLn)
     print(@sprintf("Statement Line: (%s)\n  %s\n  %s\n  %s\n  %s",
                    pl.OUTC ? "\u2714" : "\u2716",
                    pl.DATE, pl.TYPE, pl.COIN, pl.AMNT))
+end
+
+
+#--------------------------------------------------------------------------------------------------#
+#                                        Utility Functions                                         #
+#--------------------------------------------------------------------------------------------------#
+
+# Inverse constructor
+function GenSTLn(p::ParSTLn)
+    for 𝑥 in ("", "(UTC)dat,Type,Coin,Amount,Status")
+        if p == ParSTLn((x = GenSTLn(𝑥); x)); return x; end
+    end
+    𝑑, 𝑡, 𝑐, 𝑎, 𝑜 = p()
+    dStr = @sprintf("%02d/%02d/%04d %02d:%02d:%02d",
+                    month(𝑑), day(𝑑), year(𝑑),
+                    hour(𝑑), minute(𝑑), second(𝑑))
+    if 𝑡[1] == "Fee"
+        tStr = join([𝑡[1], "for", 𝑡[2]], " ")
+    elseif 𝑡[1] == "Send"
+        tStr = join([𝑡...], " ")
+    elseif 𝑡[2] != ""
+        tStr = @sprintf("%s(%s)", 𝑡...)
+    else
+        tStr = 𝑡[1]
+    end
+    cStr = @sprintf("%s", 𝑐)
+    aStr = @sprintf("%s%.10f %s", 𝑎[1] ? "-" : "+", 𝑎[2], 𝑎[3])
+    return GenSTLn(dStr, tStr, cStr, aStr,oStr)
 end
 
 
