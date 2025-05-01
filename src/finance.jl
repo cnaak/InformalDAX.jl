770b572de10adf47bfd60038958320f6ab071f99
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-04-30 22:51:04 -0300

    UPD the src/finance.jl

--- src/finance.jl
+++ src/finance.jl
@@ -2,12 +2,17 @@
 #            finance.jl - FixedPoint number for crypto finances with 10 decimal places             #
 #--------------------------------------------------------------------------------------------------#
 
-SFD =  FixedDecimal{Int64,10}   # from -922337203.6854775808
-                                # upto          0.0000000000
-                                # upto          0.0000000001
-                                # upto  922337203.6854775807
+SFD =  FixedDecimal{Int64,8}    # from -92233720368.54775808
+                                # upto            0.00000000
+                                # upto            0.00000001
+                                # upto  92233720368.54775807
 
 # export
 export SFD
 
+# Other Constants
+const CRYP_SYMB_MAX_LEN = 6
+
+# export
+export CRYP_SYMB_MAX_LEN
 
